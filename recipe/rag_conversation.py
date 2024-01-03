632b0c550eca6ec645e7a61323559bb472b05563
Ashpreet Bedi <ashpreet@phidata.com>
2024-01-03 23:51:50 +0000

    recipe/rag-conversation

---
+++ recipe/rag_conversation.py
@@ -0,0 +1,21 @@
+from phi.conversation import Conversation
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
+from phi.vectordb.pgvector import PgVector
+
+from db.session import db_url
+
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://www.family-action.org.uk/content/uploads/2019/07/meals-more-recipes.pdf"],
+    vector_db=PgVector(
+        collection="recipes",
+        db_url=db_url,
+    ),
+)
+knowledge_base.load(recreate=False)
+
+conversation = Conversation(
+    knowledge_base=knowledge_base,
+    add_references_to_prompt=True,
+)
+
+conversation.print_response("How do I make chicken tikka salad?")
