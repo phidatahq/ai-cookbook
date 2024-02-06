fbded2ff1ba5800dabf9c72d24325a5b7096edbe
Ashpreet Bedi <ashpreet@phidata.com>
2024-02-06 15:56:32 +0000

    update arxiv discord bot

--- arxiv_ai/test/bot.py
+++ arxiv_ai/test/bot.py
@@ -14,21 +14,21 @@ def run():
     intents = discord.Intents.default()
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix="/", intents=intents)
+    bot = commands.Bot(command_prefix='/', intents=intents)
 
     @bot.event
     async def on_ready():
         logger.info(f"Logged in as {bot.user}")
 
-    @bot.command(aliases=["st"])
-    async def status(ctx):
-        logger.info(f"ctx: {ctx}")
-        await ctx.send("Hi 🙉")
+    @bot.command()
+    async def discuss(ctx):
+        logger.info(f"ctx: {ctx.__dict__}")
+        logger.info(f"discussing: {ctx.message.content}")
+        await ctx.reply("Hi 🙉")
 
     @bot.event
     async def on_message(message):
-        logger.info(f"Received Message: {message}")
-
+        logger.info(f"Received Message: {message.content}")
         await bot.process_commands(message)
 
     bot.run(getenv("ARXIV_AI_TOKEN"), root_logger=True)
